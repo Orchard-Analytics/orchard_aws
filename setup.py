67d97ff6e49bc4e50836812d1574029f8d25723b
Ryan Brennan <ryantbrennan1@gmail.com>
2019-11-28 11:58:04 -0500

    name change

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='orchard_aws',
-      url='https://github.com/Orchard-Analytics/orchard-aws',
-      packages=['orchard-aws'],
+      url='https://github.com/Orchard-Analytics/orchard_aws',
+      packages=['orchard_aws'],
       version='0.1',
       description='Orchad Analytics package for interacting with AWS.',
       author='Ryan Brennan',
