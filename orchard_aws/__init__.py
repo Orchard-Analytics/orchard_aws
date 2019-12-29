0a35418f980af0d96ec682936d173b387a3d3e47
Ryan Brennan <ryantbrennan1@gmail.com>
2019-12-29 15:42:12 -0500

    bug fix

--- orchard_aws/__init__.py
+++ orchard_aws/__init__.py
@@ -2,12 +2,3 @@ from orchard_aws.s3 import *
 from orchard_aws.redshift import *
 from orchard_aws.sql_generator import *
 from orchard_aws.string_utils import *
-
-import yaml
-import os
-config_path = os.path.dirname(os.path.abspath(__file__))
-
-
-def get(config):
-    with open(config_path + '/%s.yaml' % config) as f:
-        return yaml.safe_load(f)
